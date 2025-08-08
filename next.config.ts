01633c4ecb6b8640bf16da514f5b18ad46f03e85
Derrick [Nuby] Iradukunda <72933285+derrick-nuby@users.noreply.github.com>
2025-08-08 15:46:43 +0200

    fix: restore output configuration and correct assetPrefix path

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "standalone",
-  assetsPrefix: "/web",
-  basePath: "/web",
-  webpack: (config) => { },
-
+  output: "standalone",
+  assetPrefix: "/web",
+  basePath: "/web/",
 };
 
 export default nextConfig;
