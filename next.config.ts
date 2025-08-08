95a3248c202a4be92059af03d1eab15db912accd
Derrick [Nuby] Iradukunda <72933285+derrick-nuby@users.noreply.github.com>
2025-08-08 15:36:35 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // output: "standalone",
   assetsPrefix: "/web",
-  basePath: "/web",
+  basePath: "/web/",
 };
 
 export default nextConfig;
